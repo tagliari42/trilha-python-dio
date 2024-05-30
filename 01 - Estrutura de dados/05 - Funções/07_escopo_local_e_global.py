e5a4613619061c0484cba75513213888d00f730a
Rafael Tagliari <tagliarirafael@gmail.com>
2024-05-30 19:24:29 -0300

    update: alteração em dois arquivos para refletir com mais detalhes alguns detalhes apresentados durante a aula.

--- 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
+++ 01 - Estrutura de dados/05 - Funções/07_escopo_local_e_global.py
@@ -3,8 +3,16 @@ salario = 2000
 
 def salario_bonus(bonus):
     global salario
+
+    lista_auxiliar = lista.copy()
+    lista_auxiliar.append(2)
+    print(f"lista auxiliar={lista_auxiliar}")
+
     salario += bonus
     return salario
 
 
-salario_bonus(500)  # 2500
+lista = [1]
+salario_com_bonus = salario_bonus(500)  # 2500
+print(salario_com_bonus)
+print(lista)
